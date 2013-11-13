a08c81e533cf893b780ee9b9bf7983cec2b2a58b
Dan Finnie <dan@danfinnie.com>
2013-11-12 21:53:03 -0500

    Movember widget scrapes data from their website.

--- lib/movember_fetcher.rb
+++ lib/movember_fetcher.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+
 class MovemberFetcher
   def initialize(team_number)
     @team_number = team_number
@@ -5,8 +8,32 @@ class MovemberFetcher
 
   def fetch
     {
-      label: 'Label',
-      value: 'Value'
+      label: team_name,
+      value: donations_raised
     }
+  end
+
+  private
+
+  def team_name
+    clean_selector_output('h1')
+  end
+
+  def donations_raised
+    with_units = clean_selector_output('.overflow:contains("Total Raised") .info')
+    with_units[/\s(.*)/, 1]
+  end
+
+  def clean_selector_output(selector)
+    team_page.css(selector).first.content.strip
+  end
+    
+
+  def team_page
+    @team_page ||= Nokogiri::HTML(open(team_url))
+  end
+
+  def team_url
+    "http://us.movember.com/team/#@team_number"
   end
 end
--- spec/lib/movember_fetcher_spec.rb
+++ spec/lib/movember_fetcher_spec.rb
@@ -4,11 +4,14 @@ require_relative '../../lib/movember_fetcher'
 
 describe MovemberFetcher do
   describe "#fetch" do
-    let(:fetcher) { MovemberFetcher.new(100) }
+    let(:fetcher) { MovemberFetcher.new(1005517) }
 
-    it "returns a hash suitable for a Dashing list" do
-      expect(fetcher.fetch).to have_key :label
-      expect(fetcher.fetch).to have_key :value
+    it "returns the correct team name" do
+      expect(fetcher.fetch[:label]).to eq "Pivotal - NYC"
+    end
+
+    it "returns the correct team donation total" do
+      expect(fetcher.fetch[:value]).to eq "$4,764"
     end
   end
 end

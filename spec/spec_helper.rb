8147ae3b370e85ddec3a234cd2f0f5aa8ef6bf34
Dan Finnie <dan@danfinnie.com>
2013-11-15 22:17:38 -0500

    I don't think we need this stuff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,20 +7,12 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'simple_bdd'
 
-require_relative '../lib/google_calendar'
-require_relative '../lib/local_stations'
-
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.ignore_localhost = true
 end
-
-Sinatra::Base.set :environment, :test
-Sinatra::Base.set :run, false
-Sinatra::Base.set :raise_errors, true
-Sinatra::Base.set :logging, false
 
 Capybara.current_driver = :poltergeist
 Capybara.app = Sinatra::Application.new

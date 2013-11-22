6d2d986c30a228bfa472a285dd66ea7dbdcd6b09
Brian Kelly <polymonic@gmail.com>
2013-11-21 22:49:40 -0500

    Renames local_stations to citybike_fetcher and cleans up code a bit.

--- spec/lib/citibike_fetcher_spec.rb
+++ spec/lib/citibike_fetcher_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe LocalStations, :vcr => true do
-  describe ".find" do
+describe CitibikeFetcher, :vcr => true do
+  describe '.find' do
     let(:lat_lon) { [40.74033720000001, -73.9951462] }
     let(:radius) { 0.5 }
-    let(:returned_stations) { LocalStations.find(lat_lon, radius) }
+    let(:returned_stations) { CitibikeFetcher.find(lat_lon, radius) }
 
-    it "returns the n closest stations" do
+    it 'returns the n closest stations' do
       returned_stations.map! { |station| station[:id] }
       returned_stations.should include 168
       returned_stations.should_not include 540
